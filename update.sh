cc033109bae8d2dc790085f3e00ad2e0286a519b
vird <virdvip@gmail.com>
2021-05-09 20:35:22 +0000

    update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+./stop.sh
+
+cd arweave
+git pull
+
+# seems this is optional, but if doesn't help, then only full rebuild
+# you can comment if you want faster update, but it's not for 100% cases
+rm -rf _build
+
+./rebar3 as virdpool_testnet tar
+cd ..
+
