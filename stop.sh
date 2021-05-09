321bc3eaede791d62406e21d06b57235c390d0b2
vird <virdvip@gmail.com>
2021-05-09 19:40:40 +0000

    more silent stop

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./arweave/_build/prod/rel/arweave/bin/stop
 ./arweave/_build/virdpool_testnet/rel/arweave/bin/stop
-kill `ps axww | grep node | grep proxy.coffee | awk '{print $1}'`
+kill `ps axww | grep node | grep proxy.coffee | awk '{print $1}'` 2>/dev/null
