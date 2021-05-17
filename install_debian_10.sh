4a3e5e5986b9a691f12654cad6d6e3de04e1badd
vird <virdvip@gmail.com>
2021-05-17 12:22:40 +0000

    remove atop iotop (caused some problems)

--- install_debian_10.sh
+++ install_debian_10.sh
@@ -5,7 +5,7 @@ echo "NOTE. If you think that install is too slow, you probably should not mine 
 # generic pack for almost all cryptocurrencies and comfortable work
 apt-get update
 apt-get install -y \
-  htop atop iotop screen tmux mc git nano curl wget g++ build-essential gcc make cmake autoconf automake psmisc net-tools mtr-tiny \
+  htop screen tmux mc git nano curl wget g++ build-essential gcc make cmake autoconf automake psmisc net-tools mtr-tiny \
   libboost-all-dev libssl-dev libzmq3-dev libreadline-dev libsodium-dev pkg-config libunbound-dev libtool bsdmainutils libevent-dev libminiupnpc-dev autotools-dev python3 \
   libudev-dev zlib1g-dev libseccomp-dev libcap-dev libncap-dev obfs4proxy libgmp-dev
 
