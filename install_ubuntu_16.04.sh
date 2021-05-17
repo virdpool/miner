5e2b52af8c42b83e594d444655862f9257aab04e
vird <virdvip@gmail.com>
2021-05-17 13:09:44 +0000

    unverified fix for all debian-like distro

--- install_ubuntu_16.04.sh
+++ install_ubuntu_16.04.sh
@@ -20,10 +20,59 @@ npm ci
 
 
 # arweave specific
-wget -O- https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc | apt-key add -
-echo "deb https://packages.erlang-solutions.com/ubuntu xenial contrib" | tee /etc/apt/sources.list.d/rabbitmq.list
+wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
+dpkg -i erlang-solutions_2.0_all.deb
 apt-get update
-apt-get install -y erlang
+
+apt-get install -y \
+  erlang-appmon=1:21.3.8.17-1 \
+  erlang-asn1=1:21.3.8.17-1 \
+  erlang-base=1:21.3.8.17-1 \
+  erlang-common-test=1:21.3.8.17-1 \
+  erlang-crypto=1:21.3.8.17-1 \
+  erlang-debugger=1:21.3.8.17-1 \
+  erlang-dev=1:21.3.8.17-1 \
+  erlang-dialyzer=1:21.3.8.17-1 \
+  erlang-edoc=1:21.3.8.17-1 \
+  erlang-erl-docgen=1:21.3.8.17-1 \
+  erlang-et=1:21.3.8.17-1 \
+  erlang-eunit=1:21.3.8.17-1 \
+  erlang-gs=1:21.3.8.17-1 \
+  erlang-ic=1:21.3.8.17-1 \
+  erlang-inets=1:21.3.8.17-1 \
+  erlang-inviso=1:21.3.8.17-1 \
+  erlang-jinterface=1:21.3.8.17-1 \
+  erlang-megaco=1:21.3.8.17-1 \
+  erlang-mnesia=1:21.3.8.17-1 \
+  erlang-mode=1:21.3.8.17-1 \
+  erlang-observer=1:21.3.8.17-1 \
+  erlang-odbc=1:21.3.8.17-1 \
+  erlang-os-mon=1:21.3.8.17-1 \
+  erlang-parsetools=1:21.3.8.17-1 \
+  erlang-percept=1:21.3.8.17-1 \
+  erlang-pman=1:21.3.8.17-1 \
+  erlang-public-key=1:21.3.8.17-1 \
+  erlang-reltool=1:21.3.8.17-1 \
+  erlang-runtime-tools=1:21.3.8.17-1 \
+  erlang-snmp=1:21.3.8.17-1 \
+  erlang-ssh=1:21.3.8.17-1 \
+  erlang-ssl=1:21.3.8.17-1 \
+  erlang-syntax-tools=1:21.3.8.17-1 \
+  erlang-test-server=1:21.3.8.17-1 \
+  erlang-toolbar=1:21.3.8.17-1 \
+  erlang-tools=1:21.3.8.17-1 \
+  erlang-tv=1:21.3.8.17-1 \
+  erlang-typer=1:21.3.8.17-1 \
+  erlang-wx=1:21.3.8.17-1 \
+  erlang-xmerl=1:21.3.8.17-1 \
+  erlang=1:21.3.8.17-1 \
+  erlang-diameter=1:21.3.8.17-1 \
+  erlang-eldap=1:21.3.8.17-1 \
+  erlang-ftp=1:21.3.8.17-1 \
+  erlang-tftp=1:21.3.8.17-1 \
+  erlang-ic-java=1:21.3.8.17-1 \
+  erlang-src=1:21.3.8.17-1 \
+  erlang-examples=1:21.3.8.17-1
 
 git clone --recursive --branch=miner_experimental https://github.com/virdpool/arweave
 cd arweave
--- install_ubuntu_20.04.sh
+++ install_ubuntu_20.04.sh
@@ -21,7 +21,7 @@ npm ci
 
 # arweave specific
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
-sudo dpkg -i erlang-solutions_2.0_all.deb
+dpkg -i erlang-solutions_2.0_all.deb
 apt-get update
 
 apt-get install -y \
