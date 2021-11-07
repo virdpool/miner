3c4cb882594bc670556d50fe9dfc9baa0a8ef658
vird <virdvip@gmail.com>
2021-11-07 05:22:40 +0000

    2.5.0.0

--- install_fedora_34.sh
+++ install_fedora_34.sh
@@ -26,7 +26,7 @@ yum update -y
 # yum install -y erlang-22.3.4.16-1.el8
 yum install -y erlang-23.3.1-1.fc34
 
-git clone --recursive --branch=miner_experimental_2.4.2.0 https://github.com/virdpool/arweave
+git clone --recursive --branch=miner_experimental_2.5.0.0 https://github.com/virdpool/arweave
 cd arweave
 ./rebar3 as prod tar
 
